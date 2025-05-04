4a42e62aa4ccd23d6ce9a3f736cb3e28381f83ec
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2025-05-04 04:15:53 +0300

    derlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
-import Order from "./Order/order.jsx"
-function App() {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Order/Header.jsx';
+import Order from './Order/order.jsx';
+import Main from './Order/main.jsx';
+import Success from './Order/success.jsx';
 
+function App() {
   return (
-    <>
-     <Order/>
-    </>
-  )
+    <div className="app-container">
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        
+        <Route path="/order">
+          <Header />
+          <Order />
+        </Route>
+        
+        <Route path="/success">
+          <Success />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
